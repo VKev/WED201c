811233d3df7909ce62541e29a08487cac0697fd7
VKev <huynhkhang7452@gmail.com>
2023-08-20 18:15:05 +0700

    background effect

---
+++ main.js
@@ -0,0 +1,29 @@
+
+//var particles = document.querySelectorAll(".particle");
+
+//var root = document.querySelector(".particle");
+
+//particles.forEach(element => {
+//    element.addEventListener("animationiteration", () => {
+//        var random = Math.floor(Math.random() * 500)+50 +'px';
+//        root.style.setProperty('--randomFloat1', random);
+//        console.log(random);
+//    });
+//    
+//});
+
+
+function setRandom(string,min, max, donvi, name){
+    var particles = document.querySelectorAll(name);
+    particles.forEach(par => {
+        par.style.setProperty(string,  Math.floor(Math.random() *(max-min))+min +donvi);
+
+        par.addEventListener("animationiteration", () => {
+            par.style.setProperty(string, Math.floor(Math.random() *(max-min))+min +donvi);
+        });
+    });
+}
+
+
+setRandom('--randomFloat-tx',10,90,'vw',".particle");
+setRandom('--randomFloat-size',7,17,'vh',".particle");
