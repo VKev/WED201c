bb55ef693584e6154efc94100de54d37506305cc
VKev <huynhkhang7452@gmail.com>
2023-10-17 00:15:28 +0700

    update

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function setRandom(string, min, max, donvi, name) {
+    var particles = document.querySelectorAll(name);
+    particles.forEach((par) => {
+      par.style.setProperty(
+        string,
+        Math.floor(Math.random() * (max - min)) + min + donvi
+      );
+  
+      par.addEventListener("animationiteration", () => {
+        par.style.setProperty(
+          string,
+          Math.floor(Math.random() * (max - min)) + min + donvi
+        );
+      }); 
+    });
+  }
+  
+  setRandom("--randomFloat-tx", 10, 90, "vw", ".particle");
+  setRandom("--randomFloat-size", 7, 17, "vh", ".particle");
+  
