5f0af51830a8d1baed62d6701e079e77588830a4
VKev <huynhkhang7452@gmail.com>
2023-10-18 19:44:16 +0700

    update

--- script.js
+++ script.js
@@ -5,6 +5,56 @@ function toggleMenu(){
     icon.classList.toggle("open")
 }
 
+
+let currentPage = location.href;
+let page = ["#main","#about", "#project", "#contact"];
+// listen for changes
+setInterval(function()
+{
+    if (currentPage != location.href)
+    {
+        // page has changed, set new page as 'current'
+        currentPage = location.href;
+        
+        // do your thing..
+    }
+}, 500);
+
+function upArrow(){
+  var stringindex;
+  var elementindex;
+  for( elementindex=0 ; elementindex<page.length; elementindex++ ){
+    stringindex = currentPage.indexOf(page[elementindex]);
+    if(stringindex >= 0){
+      break;
+    }
+  }
+  if(stringindex != -1 && elementindex>0){
+    var newpage = currentPage.replace(page[elementindex], page[elementindex-1]);
+    location.href= newpage;
+  }
+}
+
+function downArrow(){
+  var stringindex = currentPage.indexOf(page[0]);
+  
+  var elementindex;
+  for( elementindex=0 ; elementindex<page.length; elementindex++ ){
+    stringindex = currentPage.indexOf(page[elementindex]);
+    if(stringindex >= 0){
+      break;
+    }
+  }
+  if(stringindex != -1 && elementindex<page.length-1){
+    newpage = currentPage.replace(page[elementindex], page[elementindex+1]);
+    location.href= newpage;
+  }else{
+    newpage = currentPage.concat("/");
+    newpage = newpage.concat(page[1]);
+    location.href= newpage;
+  }
+}
+
 function toggleGmail(){
   const gmailaddress = document.querySelector(".gmail-address");
   gmailaddress.classList.toggle("open")
